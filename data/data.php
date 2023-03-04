b6b0276a10f1095a7e517c640ec8dd94ca5d9ebb
tufik <tufikhasan05@gmail.com>
2023-03-04 15:49:05 +0600

    product filters

---
+++ data/data.php
@@ -0,0 +1,59 @@
+<?php
+$products = [
+    [
+        "id"       => 1,
+        "name"     => "BENGOO G9000 Stereo Gaming Headset",
+        "image"    => "https://m.media-amazon.com/images/I/61CGHv6kmWL._AC_UY218_.jpg",
+        "price"    => "23.84",
+        "category" => "headphone",
+    ],
+    [
+        "id"       => 2,
+        "name"     => "SteelSeries Apex 3 TKL RGB Gaming Keyboard",
+        "image"    => "https://m.media-amazon.com/images/I/81dLH5W903L._AC_SX679_.jpg",
+        "price"    => "39.98",
+        "category" => "keyboard",
+    ],
+    [
+        "id"       => 3,
+        "name"     => "Logitech G502 HERO High Performance Wired Gaming Mouse",
+        "image"    => "https://m.media-amazon.com/images/I/61mpMH5TzkL._AC_UY218_.jpg",
+        "price"    => "20.84",
+        "category" => "mouse",
+    ],
+    [
+        "id"       => 4,
+        "name"     => "Gaming Chair",
+        "image"    => "https://m.media-amazon.com/images/I/61xNKvGmStL._AC_UL320_.jpg",
+        "price"    => "142.12",
+        "category" => "chair",
+    ],
+    [
+        "id"       => 5,
+        "name"     => "Gaming Headset Wireless",
+        "image"    => "https://m.media-amazon.com/images/I/61GJQyjIIhL._AC_UL320_.jpg",
+        "price"    => "30.98",
+        "category" => "headphone",
+    ],
+    [
+        "id"       => 6,
+        "name"     => "Logitech G213 Prodigy Gaming Keyboard",
+        "image"    => "https://m.media-amazon.com/images/I/61Nt8geXzWL._AC_UY218_.jpg",
+        "price"    => "49.99",
+        "category" => "keyboard",
+    ],
+    [
+        "id"       => 7,
+        "name"     => "Logitech G502 Lightspeed Wireless Gaming Mouse",
+        "image"    => "https://m.media-amazon.com/images/I/718b9wK3eaL._AC_UL320_.jpg",
+        "price"    => "45.32",
+        "category" => "mouse",
+    ],
+    [
+        "id"       => 8,
+        "name"     => "MoNiBloom Gaming Chair",
+        "image"    => "https://m.media-amazon.com/images/I/51hbFrEKiQL._AC_UL320_.jpg",
+        "price"    => "111.12",
+        "category" => "chair",
+    ],
+];
--- index.php
+++ index.php
@@ -1,64 +1,43 @@
-<?php include "./class.php";?>
+<?php
+include_once "./data/data.php";
+include_once "./function.php";
+$obj = new Products();
+?>
 <!doctype html>
 <html lang="en">
 
 <head>
-    <title>Hex to RGB/RGBA - Color Converter</title>
-    <!-- Required meta tags -->
+    <title>Products filters</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
+    <link rel="shortcut icon" href="https://icon-library.com/images/icon-for-products/icon-for-products-20.jpg" type="image/x-icon">
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
-    <!-- Font-awesome cdn -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css" />
 </head>
 
-<body style="background: url('https://static.vecteezy.com/system/resources/previews/002/072/154/original/colorful-dynamic-background-free-vector.jpg') no-repeat center;background-size: cover;">
-    <div class="container d-flex align-items-start justify-content-center mt-5" style="height: 100vh;">
-        <div class="card p-3" style="width: 380px;background: rgba(0,0,0,0.3);">
-            <h4 class="text-center text-light m-3">Hexa to RGB & RGBA</h4>
-            <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="POST" class="was-validated">
-                <div class="form-group">
-                    <input maxlength="7" class="form-control text-light" type="text" name="colorCode" style="background: rgba(0,0,0,0.3);" value="#000000" required>
-                    <div class="valid-feedback">&#10004;</div>
-                    <div class="invalid-feedback">Please enter hexadecimal color code</div>
+<body>
+    <h1 class="text-center text-dark font-weight-bold text-capitalize my-3"><a href="./">Products Filters</a></h1>
+    <div class="container">
+        <div class="row">
+            <form class="form-inline mx-auto" action="<?php echo $_SERVER['PHP_SELF'] ?>" method="get">
+                <div class="input-group mb-3">
+                    <input type="text" class="form-control" name="category" placeholder="Filter by category...">
+                    <div class="input-group-append">
+                        <button type="submit" class="btn btn-primary mb-2">Filter</button>
+                    </div>
                 </div>
-                <input class="btn btn-primary btn-block" type="submit" value="Convert Hexadecimal to RGB">
             </form>
-            <div>
-                <h6 class="mt-3 mb-1 text-light">RGB :</h6>
-                <div class="input-group">
-                    <input type="text" class="form-control text-light" style="background: rgba(0,0,0,0.3)" id="rgb" value="<?php echo $rgb; ?>">
-                    <div class="input-group-append">
-                        <span style="cursor: pointer;" class="input-group-text" onclick="copyToClipboard('rgb')"><i class="far fa-copy"></i></span>
-                    </div>
-                </div>
-                <h6 class="mt-3 mb-1 text-light">RGBA :</h6>
-                <div class="input-group">
-                    <input type="text" class="form-control text-light" style="background: rgba(0,0,0,0.3)" id="rgba"  value="<?php echo $rgba; ?>">
-                    <div class="input-group-append">
-                        <span style="cursor: pointer;" class="input-group-text" onclick="copyToClipboard('rgba')"><i class="far fa-copy"></i></span>
-                    </div>
-                </div>
-                <div style="background: <?php echo $rgb; ?>; height: 50px;" class="mt-3 w-50 mx-auto"></div>
-            </div>
-
+        </div>
+        <div class="row">
+            <?php
+            if (isset($_GET['category']) && $_GET['category'] != '') {
+                $obj->filterProduct(strtolower($_GET['category']), $products);
+            } else {
+                $obj->displayProduct($products);
+            }
+            ?>
         </div>
     </div>
-    <script>
-        function copyToClipboard(id) {
-            // Get the text field
-            let copyText = document.getElementById(id);
-            // Select the text field
-            copyText.select();
-            copyText.setSelectionRange(0, 99999); // For mobile devices
-            // Copy the text inside the text field
-            navigator.clipboard.writeText(copyText.value);
-        }
-    </script>
 </body>
 
 </html>
-
-<?php #echo substr( "123456789", 0, 6 ); ?>
